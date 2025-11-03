fb995017e7966f026caae8aa117b80972546dca7
TElzada <elzada.turdubekova@alatoo.edu.kg>
2025-11-03 18:10:10 +0600

    first homework

--- app/src/main/java/com/geeks/architecture_hw1/presentation/main/MainActivity.kt
+++ app/src/main/java/com/geeks/architecture_hw1/presentation/main/MainActivity.kt
@@ -1,21 +1,44 @@
 package com.geeks.architecture_hw1.presentation.main
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.geeks.architecture_hw1.R
+import com.geeks.architecture_hw1.data.api.CountApi
+import com.geeks.architecture_hw1.data.repository.CounterRepositoryImpl
+import com.geeks.architecture_hw1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val repository = CounterRepositoryImpl(CountApi())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        updateUI()
+
+        binding.btnIncrement.setOnClickListener {
+            repository.increment()
+            updateUI("INCREMENT")
         }
+
+        binding.btnDecrement.setOnClickListener {
+            repository.decrement()
+            updateUI("DECREMENT")
+        }
+
+        binding.btnReset.setOnClickListener {
+            repository.resetCount()
+            updateUI("RESET")
+        }
+    }
+
+    private fun updateUI(operation: String? = null) {
+        val count = repository.getCount()
+        binding.tvCount.text = "Count: ${count.count}"
+        binding.tvTime.text = "Time: ${count.createdAt}"
+        binding.tvOperation.text = "Operation: ${operation ?: count.typeOfOperation}"
     }
 }
